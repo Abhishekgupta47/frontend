3d786f8bad4c4aa976da66e66743f4dceadf69b3
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-11-28 01:16:57 +0530

    working on js

---
+++ script.js
@@ -0,0 +1,14 @@
+var istatus = document.querySelector("h5");
+var addFriend =document.querySelector("#add")
+
+
+addFriend.addEventListener("click",function(){
+  istatus.innerHTML = "Friends"
+  istatus.style.color="green"
+})
+
+var removeFriend = document.querySelector("#remove")
+
+removeFriend.addEventListener("click",function(){
+  istatus.innerHTML="stranger"
+})
